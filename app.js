53825d6d56940dfcbf937e23b51aafc9cb023166
usa3amustafa <84560869+usa3amustafa@users.noreply.github.com>
2023-10-21 16:04:57 +0500

    about-us

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   Marquee3k.init()
 })
+
+const swiper = new Swiper('.swiper', {
+  modules: [EffectSlicer],
+  threshold: 5,
+  observer: true,
+  observeParents: true,
+  watchSlidesProgress: true,
+  autoplay: { disableOnInteraction: false, enabled: true },
+  grabCursor: true,
+  effect: 'slicer',
+  slidesPerGroupAuto: false,
+  pagination: {
+    clickable: true,
+    dynamicBullets: true,
+    el: '.swiper-pagination',
+  },
+  keyboard: { enabled: true },
+})
